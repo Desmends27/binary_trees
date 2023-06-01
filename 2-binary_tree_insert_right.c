a725e442dd4d625240358f2fcd3b578526e45233
Desmends27 <desmondmends27@gmail.com>
2023-06-01 04:04:16 +0000

    betty errors

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -1,9 +1,9 @@
 #include "binary_trees.h"
 /**
- * binary_tree_right - inserts a node as the right-child
+ * binary_tree_insert_right - inserts a node as the right-child
  * @parent: parent to the node to insert
  * @value: value to be placed
- * Return: pointer to created node or NULL on failure 
+ * Return: pointer to created node or NULL on failure
  * or if parent is NULL
  */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
@@ -22,5 +22,5 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 		right_child->right->parent = right_child;
 	}
 	parent->right = right_child;
-	return parent->right;
+	return (parent->right);
 }
